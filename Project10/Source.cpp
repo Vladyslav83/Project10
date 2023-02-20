4f6ca8b96617df542615d4001d9cff4ebb68c9b2
38097 <38097@DESKTOP-I2HNV92>
2023-02-20 11:31:49 +0200

    Add project files.

---
+++ Project10/Source.cpp
@@ -0,0 +1,13 @@
+#pragma hdrstop
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+//---------------------------------------------------------------------------
+
+#pragma argsused
+int i, p, j;
+double W;
+int main(int argc, char* argv[])
+{
+    cout << "Hello world ";
+}
